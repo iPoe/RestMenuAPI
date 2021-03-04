334d96faee52e133ec18b0fb4ed6e93cfddba931
Leo <leonardosawi12@gmail.com>
2021-03-04 00:36:36 -0500

    la conexion no funciona

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -5,36 +5,39 @@ import os
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import sqlalchemy
+#...
 
+# load dotenv in the base root
+APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
+dotenv_path = os.path.join(APP_ROOT, '.env')
+load_dotenv(dotenv_path)
 UPLOAD_FOLDER = '/media/leonardo/HardDisk1/9/Tec Emergentes/TAREA1/RestMenuAPI/menus_logos_restaurantes'
 
 
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///restaurantes_jwt'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///restaurantes_jwt'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'my_precious')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 api = Api(app)
 
-# PASSWORD ="AHd5MC0Ev964DPna"
-# PUBLIC_IP_ADDRESS ="34.66.50.195"
-# DBNAME ="restaurantes_jwt"
-# PROJECT_ID ="valid-keep-305204"
-# INSTANCE_NAME ="api-restaurante"
+engine = create_engine('postgresql+psycopg2://restaurantes_jwt:Monster14320@localhost:5432/')
+conn = engine.connect()
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
   
-# # configuration 
-# #app.config["SECRET_KEY"] = "yoursecretkey"
-# app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql + mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket =/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
-#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
-  
-db = SQLAlchemy(app) 
-
-
-
 #db = SQLAlchemy(app)
+
 
 
 from project.server.restaurant.views import RecursoListarRestaurantes, RecursoUnRestaurante
